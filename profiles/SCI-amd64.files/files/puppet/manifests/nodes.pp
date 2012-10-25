cb7368448e662723fbf0fc8d5254a3f41ceaf04f
Vladimir Ipatov <vs@ipatov.net>
2012-10-25 23:05:14 +0400

    add dhcpd module with auto ddns config with bind

--- profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
+++ profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
@@ -8,4 +8,5 @@ node 'sci' {
 	include common_profile, bind9_sci, approx_local, sources_list_local
 	class { timezone: zone => "Europe/Moscow", }
 	class { locale: def_locale => "en_US.UTF-8", }
+	class { dhcpd: enabled => "yes", }
 }
