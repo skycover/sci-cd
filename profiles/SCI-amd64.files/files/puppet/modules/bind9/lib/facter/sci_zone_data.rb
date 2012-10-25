a49889929d1f42e59c55aea44fd00a1eedd511f5
Vladimir Ipatov <vs@ipatov.net>
2012-10-25 23:02:49 +0400

    add ddns support to bind9 puppet module+small fixes

--- profiles/SCI-amd64.files/files/puppet/modules/bind9/lib/facter/sci_zone_data.rb
+++ profiles/SCI-amd64.files/files/puppet/modules/bind9/lib/facter/sci_zone_data.rb
@@ -8,6 +8,11 @@ Facter.add("sci_cluster_ip") do
 		%x{. /etc/sci/sci.conf; echo $CLUSTER_IP}.chomp
 	end
 end
+Facter.add("sci_cluster_revip") do
+	setcode do
+		%x{. /etc/sci/sci.conf; echo $CLUSTER_IP|awk -F. '{print $4"."$3"."$2"."$1}'}.chomp
+	end
+end
 Facter.add("sci_node1_name") do
 	setcode do
 		%x{. /etc/sci/sci.conf; echo $NODE1_NAME}.chomp
@@ -18,6 +23,11 @@ Facter.add("sci_node1_ip") do
 		%x{. /etc/sci/sci.conf; echo $NODE1_IP}.chomp
 	end
 end
+Facter.add("sci_node1_revip") do
+	setcode do
+		%x{. /etc/sci/sci.conf; echo $NODE1_IP|awk -F. '{print $4"."$3"."$2"."$1}'}.chomp
+	end
+end
 Facter.add("sci_node2_name") do
 	setcode do
 		%x{. /etc/sci/sci.conf; echo $NODE2_NAME}.chomp
@@ -26,5 +36,15 @@ end
 Facter.add("sci_node2_ip") do
 	setcode do
 		%x{. /etc/sci/sci.conf; echo $NODE2_IP}.chomp
+	end
+end
+Facter.add("sci_node2_revip") do
+	setcode do
+		%x{. /etc/sci/sci.conf; echo $NODE2_IP|awk -F. '{print $4"."$3"."$2"."$1}'}.chomp
+	end
+end
+Facter.add("sci_sci_revip") do
+	setcode do
+		%x{host sci|awk '{print $4}'|awk -F. '{print $4"."$3"."$2"."$1}'}.chomp
 	end
 end
