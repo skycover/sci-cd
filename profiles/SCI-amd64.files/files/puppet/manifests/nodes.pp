ec0ecd43199ea238fb0f64e04c2bea0236b2d453
Vladimir Ipatov <vs@ipatov.net>
2012-10-25 17:32:12 +0400

    added puppet timezone module

--- profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
+++ profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
@@ -1,7 +1,9 @@
 node 'default' {
 	include common_profile, sources_list_local
+	class { timezone: zone => "Europe/Moscow", }
 }
 
 node 'sci' {
 	include common_profile, bind9_sci, approx_local, sources_list_local
+	class { timezone: zone => "Europe/Moscow", }
 }
