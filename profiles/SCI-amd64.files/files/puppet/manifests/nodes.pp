a0b881a73a200b784a5f8beb18e7b97f64a3c3fd
Vladimir Ipatov <vs@ipatov.net>
2012-11-07 01:20:06 +0400

    set default locale to ru_RU.UTF-8 and set actual locales list

--- profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
+++ profiles/SCI-amd64.files/files/puppet/manifests/nodes.pp
@@ -1,11 +1,11 @@
 node 'default' {
 	include common_profile, sources_list_local
 	class { timezone: zone => "Europe/Moscow", }
-	class { locale: def_locale => "en_US.UTF-8", }
+	class { locale: def_locale => "ru_RU.UTF-8", }
 }
 
 node 'sci' {
 	include common_profile, bind9_sci, approx_local, sources_list_local
 	class { timezone: zone => "Europe/Moscow", }
-	class { locale: def_locale => "en_US.UTF-8", }
+	class { locale: def_locale => "ru_RU.UTF-8", }
 }
