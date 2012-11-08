9af77fd5adb5d2beba42d41f48815d17e531ef46
Vladimir Ipatov <vs@ipatov.net>
2012-11-08 05:49:06 +0400

    add puppet module for isc-dhcp-server with ddns support

---
+++ profiles/SCI-amd64.files/files/puppet/modules/dhcpd/manifests/init.pp
@@ -0,0 +1,31 @@
+class dhcpd(enabled=yes) {
+if $enabled==yes {
+        package {isc-dhcp-server: ensure=> installed}
+}
+else {
+        package {isc-dhcp-server: ensure=> removed}
+}
+
+        file { "/etc/dhcp/dhcpd.conf.puppet":
+                owner => "root",
+                group => "root",
+                mode => 600,
+                content => template("dhcpd/dhcpd.conf.erb"),
+                require => Package['isc-dhcp-server'],
+
+        }
+
+
+        exec { "/bin/cp -a /etc/dhcp/dhcpd.conf.puppet /etc/dhcp/dhcpd.conf":
+                subscribe => File[ "/etc/dhcp/dhcpd.conf.puppet" ],
+                refreshonly => true,
+                require => Package['isc-dhcp-server'],
+        }
+
+
+        exec { "/etc/init.d/isc-dhcp-server restart":
+                subscribe => File[ "/etc/dhcp/dhcpd.conf.puppet" ],
+                refreshonly => true,
+                require => Package['isc-dhcp-server'],
+        }
+}
