6c0f87ff17e6516305ae92f3d20f8fa93e8aaa49
Vladimir Ipatov <vs@ipatov.net>
2014-08-05 15:59:59 +0400

    fix sudoers for nut

--- profiles/SCI-amd64.files/postinst.sh
+++ profiles/SCI-amd64.files/postinst.sh
@@ -392,11 +392,41 @@ cp -r files/ganeti/hooks $target/etc/ganeti/
 
 cp files/sbin/* $target/usr/local/sbin/
 
-## Add nut config
+## Add nut configs
 cp files/nut/* $target/etc/nut
 chown root:nut $target/etc/nut/*
 chmod 640 $target/etc/nut/*
-echo "%nut    ALL=NOPASSWD: /usr/local/sbin/gnt-node-shutdown.sh" >> $target/etc/sudoers
+cat <<EOF > $target/etc/sudoers
+#
+# This file MUST be edited with the 'visudo' command as root.
+#
+# Please consider adding local content in /etc/sudoers.d/ instead of
+# directly modifying this file.
+#
+# See the man page for details on how to write a sudoers file.
+#
+Defaults	env_reset
+Defaults	mail_badpass
+Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+# Host alias specification
+
+# User alias specification
+
+# Cmnd alias specification
+
+# User privilege specification
+root	ALL=(ALL:ALL) ALL
+
+# Allow members of group sudo to execute any command
+%sudo	ALL=(ALL:ALL) ALL
+
+# See sudoers(5) for more information on "#include" directives:
+
+#includedir /etc/sudoers.d
+%nut    ALL=NOPASSWD: /usr/local/sbin/gnt-node-shutdown.sh
+
+EOF
 
 # Write motd
 cat <<EOF >$target/etc/motd
